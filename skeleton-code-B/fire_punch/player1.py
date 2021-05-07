ef4ea2deac93b87d254de46bb8637a56d23fba75
kaili <kai1@student.unimelb.edu.au>
2021-05-07 22:19:00 +1000

    backup for double oracle

--- skeleton-code-B/fire_punch/player1.py
+++ skeleton-code-B/fire_punch/player1.py
@@ -11,6 +11,8 @@ from fire_punch.utils import get_expected_value
 from fire_punch.utils import estimate_evaluation
 from fire_punch.utils import new_turn
 from fire_punch.utils import find_abitary_move
+from fire_punch.utils import simulate_turn
+import random
 
 import numpy as np
 
@@ -168,25 +170,39 @@ def alpha_beta_minimax(state, depth, max_player, side, alpha, beta, count=0):
 
 
 # -------------------------------------------double oracle---------------------------------------------
-def double_oracle(state, alpha, beta, side):
+def double_oracle(state, alpha, beta, side, depth):
     utility = 0
 
     oppnent = 0
     if not side:
         oppnent = 1
 
-    if check_win(state):
+    if check_win(state) or depth == 0:
         utility = evaluation(state, side)
         print("cutoff test")
         return utility, state
 
-    max_val = float('-inf')
-    min_val = float('inf')
-    left_bound, move = alpha_beta_minimax(state, 2, True, side, max_val, min_val)
-    right_bound = alpha_beta_minimax(state, 2, False, side, max_val, min_val)[0]
-    if left_bound == right_bound:
+    max_val = -100
+    min_val = 100
+
+    max_move = []
+    min_move = []
+
+    for upper in find_legal_operations(state,1).values():
+        max_move += upper
+    for lower in find_legal_operations(state,0).values():
+        min_move += lower
+    
+    max_move = random.sample(max_move,len(max_move)//5)
+
+    min_move = random.sample(min_move,len(min_move)//5)
+
+
+    left_bound, move = alpha_beta_minimax_limit(state, 2, True, side, max_val, min_val, max_move, min_move)
+    right_bound = alpha_beta_minimax_limit(state, 2, False, side, max_val, min_val, max_move, min_move)[0]
+    if left_bound == right_bound or abs(left_bound - right_bound) < 1.1:
         utility = left_bound
-        print("alpha = beta")
+        print("left {a} = right {b} ".format(a = left_bound, b = right_bound))
         return utility, move
     # find arbitrary move
     my_move = []
@@ -195,9 +211,9 @@ def double_oracle(state, alpha, beta, side):
     ad_move += find_abitary_move(state, oppnent)
 
     # key : actions i, value : [ui,j]
-    pIJ = alpha_beta_minimax_limit(state, 2, False, side, max_val, min_val, copy.deepcopy(my_move),
+    pIJ = alpha_beta_minimax_limit(state, 2, True, side, max_val, min_val, copy.deepcopy(my_move),
                                    copy.deepcopy(ad_move))[0]
-    oIJ = alpha_beta_minimax_limit(state, 2, True, side, max_val, min_val, copy.deepcopy(my_move),
+    oIJ = alpha_beta_minimax_limit(state, 2, False, side, max_val, min_val, copy.deepcopy(my_move),
                                    copy.deepcopy(ad_move))[0]
 
     # initialize the boundary of the first abitary actions
@@ -207,7 +223,7 @@ def double_oracle(state, alpha, beta, side):
 
     print("pIJ = {a}, oIJ = {b}, oij = {c}, pij ={d} ".format(a=pIJ, b=oIJ, c=o[0][0], d=p[0][0]))
 
-    while alpha != beta:
+    while alpha != beta or abs(alpha - beta) > 0.5 or len(my_move) < 10:
         print("enter_while")
 
         index_i = 0
@@ -218,9 +234,10 @@ def double_oracle(state, alpha, beta, side):
             for j in ad_move:
 
                 if p[index_i][index_j] < o[index_i][index_j]:
-                    new_state = simultaneous_move(state, i, j, side)
 
-                    u_temp = double_oracle(new_state, alpha, beta, side)[0]
+                    new_state = new_turn(side,state, i, j)
+
+                    u_temp = double_oracle(new_state, alpha, beta, side, depth - 1)[0]
 
                     p[index_i][index_j] = u_temp
                     o[index_i][index_j] = u_temp
@@ -266,22 +283,29 @@ def double_oracle(state, alpha, beta, side):
 
 
 def BR_max(state, alpha, y, side):
+    print("{a}  {b} ".format(a="br max", b=len(y)))
     br = alpha
     move = None
+
+    maxval = -100
+    minval = 100
 
     action_to_maxmal = []
 
     for actions in find_legal_operations(state, side).values():
         action_to_maxmal += actions
+    
+    action_to_maxmal = random.sample(action_to_maxmal, len(action_to_maxmal)//5)
 
     p = []
     o = []
 
     for i in range(0, len(action_to_maxmal)):
 
-        piJ = alpha_beta_minimax_limit(state, 2, False, side, float('-inf'), float('inf'), [action_to_maxmal[i]],
+        piJ = alpha_beta_minimax_limit(state, 2, True, side,  maxval, minval, [action_to_maxmal[i]],
                                        list(y.keys()))[0]
-        oiJ = alpha_beta_minimax_limit(state, 2, True, side, float('-inf'), float('inf'), [action_to_maxmal[i]],
+
+        oiJ = alpha_beta_minimax_limit(state, 2, False, side, maxval, minval, [action_to_maxmal[i]],
                                        list(y.keys()))[0]
 
         # initialise the boudary
@@ -296,22 +320,18 @@ def BR_max(state, alpha, y, side):
         if list(y.values())[j] > 0 and p[j] < o[j]:
 
             for action, prob in y.items():
+                pij_estimate = max(p[j], simulate_turn(side,state,action_to_maxmal[i], action))
 
-                pij_estimate = max(p[j], br - estimate_evaluation(y, o[j], action))
+                utility[j] = pij_estimate
 
-                if pij_estimate > o[j]:
-                    continue
-                else:
-                    new_state = new_turn(side, state, action_to_maxmal[i], action)
-                    utility[j] = double_oracle(new_state, p[j], o[j])[0]
-                    p[j] = utility[j]
-                    o[j] = utility[j]
 
         # if have been calculate
 
         expected = get_expected_value(y, utility)
+        
 
         if expected > br:
+            print("expected value of y : {a} with move {b}".format(a = expected, b = action_to_maxmal[i]))
             move = action_to_maxmal[i]
             br = expected
 
@@ -319,8 +339,12 @@ def BR_max(state, alpha, y, side):
 
 
 def BR_min(state, beta, x, side):
+    print("{a}  {b} ".format(a="br min", b=len(x)))
     br = beta
     move = None
+
+    maxval = -100
+    minval = 100
 
     action_to_minimal = []
 
@@ -332,9 +356,9 @@ def BR_min(state, beta, x, side):
 
     for i in range(0, len(action_to_minimal)):
 
-        piJ = alpha_beta_minimax_limit(state, 2, False, side, float('-inf'), float('inf'), [action_to_minimal[i]],
+        piJ = alpha_beta_minimax_limit(state, 2, True, side, maxval, minval, [action_to_minimal[i]],
                                        list(x.keys()))[0]
-        oiJ = alpha_beta_minimax_limit(state, 2, True, side, float('-inf'), float('inf'), [action_to_minimal[i]],
+        oiJ = alpha_beta_minimax_limit(state, 2, False, side, maxval, minval, [action_to_minimal[i]],
                                        list(x.keys()))[0]
 
         # initialise the boudary
@@ -349,34 +373,22 @@ def BR_min(state, beta, x, side):
 
             for action, prob in x.items():
 
-                pij_estimate = min(o[j], br - estimate_evaluation(x, p[j], action))
+                #pij_estimate = min(o[j], br - estimate_evaluation(x, p[j], action))
+                pij_estimate = max(p[j], simulate_turn(side,state,action_to_minimal[i], action))
 
-                if pij_estimate < p[j]:
-                    continue
-                else:
-                    new_state = new_turn(side, state, action_to_minimal[i], action)
-                    utility[j] = double_oracle(new_state, p[j], o[j])[0]
-                    p[j] = utility[j]
-                    o[j] = utility[j]
+                utility[j] = pij_estimate
+
 
         # if have been calculate
-
+        
         expected = get_expected_value(x, utility)
+        
         if expected < br:
+            print("expected value of x : {a} with move {b}".format(a = expected, b = action_to_minimal[i]))
             move = action_to_minimal[i]
             br = expected
 
     return move, br
-
-
-def simultaneous_move(state, move1, move2, side):
-    new_state = copy.deepcopy(state)
-    new_state.operate(move1, side)
-    new_state.operate(move2, 1 - side)
-    new_state.battle(move1[2])
-    new_state.battle(move2[2])
-
-    return new_state
 
 
 # -------------------------------------------helper---------------------------------------------
@@ -422,11 +434,137 @@ def check_win(state):
     return flag
 
 
+'''
+def BR_max(state, alpha, y, side):
+    print("{a}  {b} ".format(a="br max", b=len(y)))
+    br = alpha
+    move = None
+
+    maxval = -100
+    minval = 100
+
+    action_to_maxmal = []
+
+    for actions in find_legal_operations(state, side).values():
+        action_to_maxmal += actions
+    
+    action_to_maxmal = random.sample(action_to_maxmal, len(action_to_maxmal)//5)
+
+    p = []
+    o = []
+
+    for i in range(0, len(action_to_maxmal)):
+
+        piJ = alpha_beta_minimax_limit(state, 2, True, side,  maxval, minval, [action_to_maxmal[i]],
+                                       list(y.keys()))[0]
+
+        oiJ = alpha_beta_minimax_limit(state, 2, False, side, maxval, minval, [action_to_maxmal[i]],
+                                       list(y.keys()))[0]
+
+        # initialise the boudary
+        p = [piJ for i in range(0, len(y))]
+        o = [oiJ for i in range(0, len(y))]
+
+        utility = [-1000 for i in range(0, len(y))]
+        utility = np.array(utility)
+
+        j = 0
+
+        if list(y.values())[j] > 0 and p[j] < o[j]:
+
+            for action, prob in y.items():
+                pij_estimate = max(p[j], simulate_turn(side,state,action_to_maxmal[i], action))
+
+                #pij_estimate = max(p[j], br - estimate_evaluation(y, o[j], action))
+                #print(pij_estimate)
+                
+
+                if pij_estimate > o[j]:
+                    continue
+                else:
+                    #new_state = new_turn(side, state, action_to_maxmal[i], action)
+                    #utility[j] = double_oracle(new_state, p[j], o[j], side, 3)[0]
+
+                    utility[j] = pij_estimate
+                    p[j] = utility[j]
+                    o[j] = utility[j]
+
+        # if have been calculate
+
+        expected = get_expected_value(y, utility)
+        print(expected)
+
+        if expected > br:
+            move = action_to_maxmal[i]
+            br = expected
+
+    return move, br
+
+
+def BR_min(state, beta, x, side):
+    print("br min")
+    br = beta
+    move = None
+
+    maxval = -100
+    minval = 100
+
+    action_to_minimal = []
+
+    for actions in find_legal_operations(state, side).values():
+        action_to_minimal += actions
+
+    p = []
+    o = []
+
+    for i in range(0, len(action_to_minimal)):
+
+        piJ = alpha_beta_minimax_limit(state, 2, True, side, maxval, minval, [action_to_minimal[i]],
+                                       list(x.keys()))[0]
+        oiJ = alpha_beta_minimax_limit(state, 2, False, side, maxval, minval, [action_to_minimal[i]],
+                                       list(x.keys()))[0]
+
+        # initialise the boudary
+        p = [piJ for i in range(0, len(x))]
+        o = [oiJ for i in range(0, len(x))]
+
+        utility = [1000 for i in range(0, len(x))]
+        utility = np.array(utility)
+        j = 0
+
+        if list(x.values())[j] > 0 and p[j] < o[j]:
+
+            for action, prob in x.items():
+
+                #pij_estimate = min(o[j], br - estimate_evaluation(x, p[j], action))
+                pij_estimate = max(p[j], simulate_turn(side,state,action_to_minimal[i], action))
+
+                if pij_estimate < p[j]:
+                    continue
+                else:
+                    #new_state = new_turn(side, state, action_to_minimal[i], action)
+                    #utility[j] = double_oracle(new_state, p[j], o[j], side, 3)[0]
+                    utility[j] = pij_estimate
+                    p[j] = utility[j]
+                    o[j] = utility[j]
+
+        # if have been calculate
+
+        expected = get_expected_value(x, utility)
+        if expected < br:
+            move = action_to_minimal[i]
+            br = expected
+
+    return move, br
+'''
+
+
 player = Player("upper")
-player.state.operate(("THROW", "s", (4, -4)), 1)
+player.state.operate(("THROW", "r", (4, -4)), 1)
 player.state.operate(("THROW", "p", (-4, 0)), 0)
 
-print(double_oracle(player.state, -100, 100, 1))
+print(double_oracle(player.state, -100, 100, 1, 2))
+
 # print(alpha_beta_minimax_limit(player.state, 3, True, 1, float('-inf'), float('inf'), [("SLIDE", (4,-4), (3, -4))], []))
 # print(alpha_beta_minimax(player.state, 4, True, 1, float('-inf'), float('inf')))
 # print(alpha_beta_minimax(player.state, 4, False, 1, float('-inf'), float('inf')))
